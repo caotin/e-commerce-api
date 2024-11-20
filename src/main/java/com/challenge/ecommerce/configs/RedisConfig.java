fa3e2cf2c50f273de43e7c4ddcdaf8e9c4b8e682
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2024-11-20 11:41:28 +0700

    fix config redis url

--- src/main/java/com/challenge/ecommerce/configs/RedisConfig.java
+++ src/main/java/com/challenge/ecommerce/configs/RedisConfig.java
@@ -6,44 +6,54 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import java.net.URI;
+import java.net.URISyntaxException;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@EnableRedisRepositories
+@Slf4j
 public class RedisConfig {
 
-  @Value("${spring.data.redis.host}")
-  String redisHost;
-
-  @Value("${spring.data.redis.port}")
-  int redisPort;
-
-  @Value("${spring.data.redis.password}")
-  String redisPassword;
+  @Value("${spring.data.redis.url}")
+  private String redisUrl;
 
   @Bean
   public LettuceConnectionFactory lettuceConnectionFactory() {
-    RedisStandaloneConfiguration redisStandaloneConfiguration =
-        new RedisStandaloneConfiguration(redisHost, redisPort);
-    redisStandaloneConfiguration.setPassword(redisPassword);
-    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    try {
+      URI redisUri = new URI(redisUrl);
+      RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+      configuration.setHostName(redisUri.getHost());
+      configuration.setPort(redisUri.getPort());
+      
+      if (redisUri.getUserInfo() != null) {
+        configuration.setPassword(redisUri.getUserInfo().split(":", 2)[1]);
+      }
+      
+      LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+        .useSsl()
+        .build();
+      
+      return new LettuceConnectionFactory(configuration, clientConfig);
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Redis URL is invalid", e);
+    }
   }
 
   @Bean
   public RedisTemplate<String, Object> redisTemplate() {
-    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(lettuceConnectionFactory());
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
-    redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-    redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-    redisTemplate.afterPropertiesSet();
-    return redisTemplate;
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(lettuceConnectionFactory());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+    template.setHashKeySerializer(new StringRedisSerializer());
+    template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+    template.afterPropertiesSet();
+    return template;
   }
 }
