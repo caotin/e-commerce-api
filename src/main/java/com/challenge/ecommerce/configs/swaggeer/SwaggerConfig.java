764b821dc5d466cf355aa2b5aa6714f4ef0c2867
hoaigv <hoangminhhoai.gv2002@gmail.com>
2024-11-21 10:17:35 +0700

    [MODIFY]update SwaggerConfig to enable https v1

--- src/main/java/com/challenge/ecommerce/configs/swaggeer/SwaggerConfig.java
+++ src/main/java/com/challenge/ecommerce/configs/swaggeer/SwaggerConfig.java
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.ServletContext;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,9 +20,11 @@ public class SwaggerConfig {
   }
 
   @Bean
-  OpenAPI customOpenAPI() {
+  OpenAPI customOpenAPI(ServletContext servletContext) {
+    Server server = new Server().url(servletContext.getContextPath());
     return new OpenAPI()
         .info(new Info().title("API book storage online").version("1.0"))
+        .servers(List.of(server))
         .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
         .components(
             new Components()
